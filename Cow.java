2ed87b3d9d924fda8593e16104a693593ed0af9e
Bt9067 <152593667+Bt9067@users.noreply.github.com>
2024-12-25 19:16:57 +0700

    Add files via upload

---
+++ Cow.java
@@ -0,0 +1,9 @@
+public class Cow extends Animal{
+    @Override
+    void sound() {
+        System.out.println("moo moo(มอ มอ)");
+    }
+    void milk(){
+        System.out.println("... now loading...");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Animal animal = new Animal();
+        Cow wagyu = new Cow();
+        Duck donald = new Duck("โดนัลด์");
+        Duck daisy = new Duck("เดซี่");
+        Pig burin = new Pig();
+        Owl hedwig = new Owl();
+        PekingDuck fourseasons = new PekingDuck("4ss");
+
+        //wagyu.sound();
+        //burin.sound();
+        //hedwig.sound();
+        //donald.sound();
+        donald.clean(wagyu);
+        daisy.clean(donald);
+        fourseasons.clean(daisy);
+        fourseasons.fly();
+        Duck jiatongheng = new PekingDuck("jth");
+        jiatongheng.clean(daisy);
+        jiatongheng.fly();
+
+        donald.clean(new Cow());
+    }
+}
